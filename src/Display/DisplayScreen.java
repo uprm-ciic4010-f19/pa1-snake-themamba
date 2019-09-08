6773300b928f33865dab26a92945c0dbddf5190d
sofiaburgos <sofiaburgos@10.34.2.34>
2019-09-07 21:59:15 -0400

    Purple Background

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -48,7 +48,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
+        canvas.setBackground(Color.magenta);
 
         frame.add(canvas);
         frame.pack();
